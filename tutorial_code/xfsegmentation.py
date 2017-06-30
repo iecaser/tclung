e8406e72800b277f44b5a4ea1a71d7619c94930d
四环吴恩达 <iecaser@163.com>
2017-06-30 09:46:22 +0800

    # 修正了部分漏检问题 # 修正了nodeShould统计不完全问题

---
+++ tutorial_code/xfsegmentation.py
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+'''
+
+@author: Soofo
+
+@license: (C) Copyright 2017-2018, IECAS Limited.
+
+@contact: iecaser@163.com
+
+@software: pycharm
+
+@file: xfsegmentation.py
+
+@time: 17-6-23 下午7:05
+
+@desc:
+
+'''
+
+
